33c252e00d262c9933a420519f713b6f7f23bac5
Rafal Radomski <biuro@radiart.pl>
2017-06-20 14:36:08 +0100

    holes

--- src/app/molegame/hole/hole.component.ts
+++ src/app/molegame/hole/hole.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Hole} from '../hole';
 
 @Component({
      selector: 'app-hole',
   templateUrl: './hole.component.html',
     styleUrls: ['./hole.component.scss']
 })
-export class HoleComponent implements OnInit {
+export class HoleComponent {
 
-  constructor() { }
+   @Input()
 
-  ngOnInit() {
-  }
+   public hole: Hole;
+
 
 }
